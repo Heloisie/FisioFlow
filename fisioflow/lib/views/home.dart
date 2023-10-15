97264760966b9f068eaa1813b47423fe2ff00e1b
heloisie <heloisie.marcelli@dilettasolutions.com>
2023-10-15 11:19:26 -0300

    bottom navigation

---
+++ fisioflow/lib/views/home.dart
@@ -0,0 +1,56 @@
+import 'package:fisioflow/views/favorites_page.dart';
+import 'package:fisioflow/views/initial_page.dart';
+import 'package:fisioflow/views/workouts_page.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    const InitialPage(),
+    const WorkoutsPage(),
+    const FavoritesPage(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: _children[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Initial',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fitness_center),
+              label: 'Workouts',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+          ],
+        ));
+  }
+}
